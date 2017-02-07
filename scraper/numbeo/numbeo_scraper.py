9425e82d5dbc900690939ea5973b72f12a3791fc
a-leut <turbomonkeyninja@gmail.com>
2017-02-07 10:23:24 -0700

    refactor to selenium for submit, close popup

--- scraper/numbeo/numbeo_scraper.py
+++ scraper/numbeo/numbeo_scraper.py
@@ -1,4 +1,6 @@
 from splinter import Browser
+from selenium.webdriver.common.keys import Keys
+import os
 from scraper import rand_wait
 from time import sleep
 
@@ -7,16 +9,26 @@ class NumbeoScraper(object):
     """ Scrapes numbeo.com for cost of living data
     """
     def get_html_for_city(self, city):
-        with Browser('chrome') as browser:
+        with Browser('chrome') as b:
             # visit home page
-            browser.visit('https://www.numbeo.com/cost-of-living/')
+            b.visit('https://www.numbeo.com/cost-of-living/')
 
             # fill search form with city
-            search_xp = '//*[@id="dispatch_form"]'
-            rand_wait(browser, search_xp)
-            search_form = browser.find_by_xpath(search_xp).first
-            search_form.find_by_xpath('//*[@id="city_selector_city_id"]').fill('booty')
+            rand_wait(b, '//*[@id="dispatch_form"]')
+            search_form = b.driver.find_element_by_xpath('//*[@id="city_selector_city_id"]')
+            search_form.send_keys(city)
+            sleep(3)
+            search_form.send_keys(Keys.TAB)
+            #sleep(3)
+            #search_form.send_keys(Keys.ENTER)
+            #sleep()
+            b.find_by_xpath('/html/body/div[6]/div[1]/button').first.click()
             sleep(100)
 
+    def save_page(self, browser, city):
+        with open('output.html', 'w') as f:
+            f.write(browser.html.encode('utf-8'))
+
+
 n = NumbeoScraper()
-n.get_html_for_city('c')
+n.get_html_for_city('Denver, CO')

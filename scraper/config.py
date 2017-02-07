4fa810672b98779fa2ea7c1093b668a6b97c7569
a-leut <turbomonkeyninja@gmail.com>
2017-02-07 11:24:17 -0700

    changing to city data from wikipedia

--- scraper/config.py
+++ scraper/config.py
@@ -1,5 +1,4 @@
 import os
 
 PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
-CITIES_FILE = os.path.join(PARENT_DIR, 'cities.json')
 NUMBEO_DIR = os.path.join(PARENT_DIR, 'data', 'numbeo')

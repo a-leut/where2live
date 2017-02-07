ae1838f97ab7651598983952fb6da4255dd09b64
a-leut <turbomonkeyninja@gmail.com>
2017-02-07 13:05:00 -0700

    scraper up

--- scraper/numbeo/numbeo_scraper.py
+++ scraper/numbeo/numbeo_scraper.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import errno
 import time
+import random
 import splinter
 from selenium.webdriver.common.keys import Keys
 from scraper import rand_wait_for_element
@@ -12,7 +13,7 @@ class NumbeoScraper(object):
     """ Scrapes numbeo.com for cost of living data
     """
     def __init__(self, browser_type, force_reset=False):
-        self.t_file = os.path.join(NUMBEO_DIR, 'log.txt')
+        self.log_file = os.path.join(NUMBEO_DIR, 'log.txt')
         # Make sure output dirs are set up
         try:
             os.makedirs(NUMBEO_DIR)
@@ -21,24 +22,43 @@ class NumbeoScraper(object):
                 pass
             else:
                 raise
-        # Delete tracked file if force reset
+        # Delete tracked log if force reset
         if force_reset:
             try:
-                os.remove(self.t_file)
+                os.remove(self.log_file)
             except OSError:
                 pass
-        # Create new tracked file if it doesnt exist
-        if not os.path.isfile(self.t_file):
-            with open(self.t_file, 'w') as f:
+        # Create new tracked log if it doesnt exist
+        if not os.path.isfile(self.log_file):
+            with open(self.log_file, 'w') as f:
                 f.write('# Numbeo scrape log\n# Started: {0}\n'.format(utc_timestamp()))
         self.browser_type = browser_type
 
-    def scrape_cities(self, cities):
+    def scrape_cities(self, target_cities):
         """ Search for list of cities on numbeo and save output to dir
         """
-        for city in cities:
-            html = self.get_html_for_city(city)
-            self.save_page(html, city)
+        # Find which cities are already scraped from log
+        with open(self.log_file, 'r') as f:
+            log_lines = f.readlines()[2:]
+            scraped_cities = [l.strip() for l in log_lines]
+        new_cities = set(target_cities).difference(scraped_cities)
+        new_cities.discard('')  # newline not a city
+        total_new = len(new_cities)
+
+        while len(new_cities) > 0:
+            target_city = random.choice(tuple(new_cities))
+            print('Scraping numbeo for {0} - {1}/{2}'.format(
+                target_city, total_new - len(new_cities) + 1, total_new
+            ))
+
+            #html = self.get_html_for_city(city)
+            #self.save_page(html, city)
+            with open(self.log_file, 'a') as f:
+                f.write('{0}\n'.format(target_city))
+            new_cities.discard(target_city)
+
+        print('All cities scraped, complete!')
+
 
     def get_html_for_city(self, city):
         """ Launch browser, search for cities, and return html
--- scraper/numbeo/run_numbeo_scrape.py
+++ scraper/numbeo/run_numbeo_scrape.py
@@ -6,7 +6,7 @@ def main():
     # load cities
     with open(CITIES_FILE, 'r') as f:
         cities = f.read().split('\n')[1:]
-    scraper = NumbeoScraper('chrome')
+    scraper = NumbeoScraper('phantomjs')
     scraper.scrape_cities(cities)
 
 if __name__ == '__main__':

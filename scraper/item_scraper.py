c53270ea37811bdd8dc0e020de4d3a4039498b22
a-leut <turbomonkeyninja@gmail.com>
2017-02-09 13:56:25 -0700

    implemented base, broke numbeo

---
+++ scraper/item_scraper.py
@@ -0,0 +1,75 @@
+import os
+import errno
+import random
+import time
+from scraper.util import utc_timestamp
+
+
+class ItemScraper(object):
+    """ Base class to index a website and download some html pages. Enumerates
+        an input list and runs some action per input.
+    """
+    def __init__(self, browser_type, log_dir, force_reset=False):
+        self.dir = log_dir
+        self.log_file = os.path.join(log_dir, '{0}_log.txt'.format(type(self)))
+        self.browser_type = browser_type
+        # Make sure output dirs are set up
+        try:
+            os.makedirs(log_dir)
+        except OSError as e:
+            if e.errno == errno.EEXIST and os.path.isdir(log_dir):
+                pass
+            else:
+                raise
+        # Delete tracked log if force reset
+        if force_reset:
+            try:
+                os.remove(self.log_file)
+            except OSError:
+                pass
+        # Create new tracked log if it doesnt exist
+        if not os.path.isfile(self.log_file):
+            with open(self.log_file, 'w') as f:
+                f.write('# {0} scrape log\n# Started: {1}\n'.
+                        format(type(self), utc_timestamp()))
+
+    def scrape_cities(self, target_items):
+        """ Search for list of cities on numbeo and save output to dir
+        """
+        # Find which cities are already scraped from log
+        with open(self.log_file, 'r') as f:
+            log_lines = f.readlines()[2:]
+        scraped_items = [l.strip() for l in log_lines]
+        new_items = set(target_items).difference(scraped_items)
+        new_items.discard('')  # newline not a city
+        total_new = len(new_items)
+        while len(new_items) > 0:
+            # Choose random item
+            new_item = random.choice(tuple(new_items))
+            print('Scraping numbeo for {0} - {1}/{2}'.format(
+                new_item, total_new - len(new_items) + 1, total_new
+            ))
+            # Save html for item
+            html = self.get_html_for_item(new_item)
+            self.save_page(html, new_item)
+            with open(self.log_file, 'a') as f:
+                f.write('{0}\n'.format(new_item))
+            new_items.discard(new_item)
+            # Wait patiently to re-index
+            time.sleep(random.randrange(4, 20))
+        print('All items scraped, complete!')
+
+    def save_page(self, html, item):
+        filename = '{0}_{1}.html'.format(
+           city.replace(',', '').replace(' ', '-'), utc_timestamp()
+        )
+        with open(os.path.join(NUMBEO_DIR, filename), 'w', encoding='utf-8') as f:
+            f.write(html)
+
+    def get_html_for_item(self, item):
+        """ Launch browser, get page for item and, and return html
+        """
+        raise NotImplementedError('Not implemented.')
+
+    def save_page(self, html, item):
+        raise NotImplementedError('Not implemented.')
--- scraper/numbeo/numbeo_scraper.py
+++ scraper/numbeo/numbeo_scraper.py
@@ -1,66 +1,13 @@
-import os
-import errno
 import time
-import random
 import splinter
 from selenium.webdriver.common.keys import Keys
 from scraper import rand_wait_for_element
-from scraper.util import utc_timestamp
-from scraper.settings import CITIES_FILE, NUMBEO_DIR
+from scraper.settings import CITIES_FILE
+from scraper.item_scraper import ItemScraper
 
-
-class NumbeoScraper(object):
+class NumbeoScraper(ItemScraper):
     """ Scrapes numbeo.com for cost of living data
     """
-    def __init__(self, browser_type, force_reset=False):
-        self.log_file = os.path.join(NUMBEO_DIR, 'log.txt')
-        # Make sure output dirs are set up
-        try:
-            os.makedirs(NUMBEO_DIR)
-        except OSError as e:
-            if e.errno == errno.EEXIST and os.path.isdir(NUMBEO_DIR):
-                pass
-            else:
-                raise
-        # Delete tracked log if force reset
-        if force_reset:
-            try:
-                os.remove(self.log_file)
-            except OSError:
-                pass
-        # Create new tracked log if it doesnt exist
-        if not os.path.isfile(self.log_file):
-            with open(self.log_file, 'w') as f:
-                f.write('# Numbeo scrape log\n# Started: {0}\n'.format(utc_timestamp()))
-        self.browser_type = browser_type
-
-    def scrape_cities(self, target_cities):
-        """ Search for list of cities on numbeo and save output to dir
-        """
-        # Find which cities are already scraped from log
-        with open(self.log_file, 'r') as f:
-            log_lines = f.readlines()[2:]
-        scraped_cities = [l.strip() for l in log_lines]
-        new_cities = set(target_cities).difference(scraped_cities)
-        new_cities.discard('')  # newline not a city
-        total_new = len(new_cities)
-
-        while len(new_cities) > 0:
-            target_city = random.choice(tuple(new_cities))
-            print('Scraping numbeo for {0} - {1}/{2}'.format(
-                target_city, total_new - len(new_cities) + 1, total_new
-            ))
-            html = self.get_html_for_city(target_city)
-            self.save_page(html, target_city)
-            with open(self.log_file, 'a') as f:
-                f.write('{0}\n'.format(target_city))
-            new_cities.discard(target_city)
-
-            time.sleep(random.randrange(4, 20))
-
-        print('All cities scraped, complete!')
-
-
     def get_html_for_city(self, city):
         """ Launch browser, search for cities, and return html
         """
@@ -80,14 +27,6 @@ class NumbeoScraper(object):
                 pass
             # Return search result
             return str(b.html)
-
-    def save_page(self, html, city):
-        filename = '{0}_{1}.html'.format(
-           city.replace(',', '').replace(' ', '-'), utc_timestamp()
-        )
-        with open(os.path.join(NUMBEO_DIR, filename), 'w', encoding='utf-8') as f:
-            f.write(html)
-
 
 def main():
     with open(CITIES_FILE, 'r') as f:
